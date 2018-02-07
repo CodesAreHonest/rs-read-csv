716f070cbdd5ef7d3dc6abb7b6c000e4ad2bd788
Yinghua <yinghuachai0@gmail.com>
2018-02-07 20:10:46 +0800

    modified nothing

--- src/main.rs
+++ src/main.rs
@@ -22,27 +22,6 @@ const LEO_DIRECTORY: &'static str = "/home/yinghua/Documents/FYP1/FYP-data/subje
 const COMPANY_DIRECTORY: &'static str = "/home/yinghua/Documents/FYP1/FYP-data/company-data/company-data-full.csv";
 const NSPL_DIRECTORY: &'static str = "/home/yinghua/Documents/FYP1/FYP-data/postcode-data/UK-NSPL.csv";
 
-//fn retrieve_data(directory: &'static str) -> Result<(), Box<Error>> {
-//
-//    // Parse the CSV reader and iterate over each record.
-//    let csv_file = File::open(directory).expect("Error open LEO file");
-//
-//    let start = PreciseTime::now();
-//    let mut rdr = csv::Reader::from_reader(csv_file);
-//
-//    for result in rdr.records() {
-//        // The iterator yields Result<StringRecord, Error>, so we check the error here.
-//        let record = result?;
-//        //        println!("{:?}", record);
-//    }
-//
-//    let end = PreciseTime::now();
-//    let duration = start.to(end);
-//
-//    println!("{} seconds on retrieve {}. ", duration, directory);
-//    Ok(())
-//}
-
 fn retrieve_data(directory: &'static str, indicator: &'static str) -> u32 {
 
     println!("BEGIN retrieve data from {} files. ", indicator);
@@ -55,7 +34,7 @@ fn retrieve_data(directory: &'static str, indicator: &'static str) -> u32 {
 
     for result in rdr.records() {
         // The iterator yields Result<StringRecord, Error>, so we check the error here.
-        let record = result;
+        let -record = result;
         //        println!("{:?}", record);
     }
 
@@ -75,25 +54,9 @@ fn sequential_read() {
 
     let start = PreciseTime::now();
 
-    let leo = retrieve_data(LEO_DIRECTORY, LEO_INDICATOR);
-    let company = retrieve_data(COMPANY_DIRECTORY, COMPANY_INDICATOR);
-    let nspl = retrieve_data(NSPL_DIRECTORY, NSPL_INDICATOR);
-
-    //    // retrieve all the data sequentially
-    //    if let Err(err) = retrieve_data(LEO_DIRECTORY) {
-    //        println!("error running retreive_leo: {}", err);
-    //        process::exit(1);
-    //    }
-    //
-    //    if let Err(err) = retrieve_data(COMPANY_DIRECTORY) {
-    //        println!("error running retreive_company: {}", err);
-    //        process::exit(1);
-    //    }
-    //
-    //    if let Err(err) = retrieve_data(NSPL_DIRECTORY) {
-    //        println!("error running retreive_postcode: {}", err);
-    //        process::exit(1);
-    //    }
+    let _leo = retrieve_data(LEO_DIRECTORY, LEO_INDICATOR);
+    let _company = retrieve_data(COMPANY_DIRECTORY, COMPANY_INDICATOR);
+    let _nspl = retrieve_data(NSPL_DIRECTORY, NSPL_INDICATOR);
 
     let end = PreciseTime::now();
     let duration = start.to(end);
@@ -147,6 +110,6 @@ fn concurrent_read() {
 }
 
 fn main() {
-    sequential_read();
+//    sequential_read();
     concurrent_read();
 }
